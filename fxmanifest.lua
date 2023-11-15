3416a10b850a024519afe4fac5d76cfa4c309312
Niklas B <68842909+justSalkin@users.noreply.github.com>
2023-11-15 09:52:07 +0100

    Patch 1.1
    - fixed problem with non spawning npc
    - fixed bug with shop prices

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'justSalkin'
 description 'Script for a traveling trapper'
-version '1.0'
+version '1.1'
 
 client_script {
     '/client/warmenu.lua',
@@ -26,4 +26,4 @@ dependencies {
     'vorp_inventory',
 }
 
--- LAST EDIT by just_Salkin 01.11.2023 | WHITE-SANDS-RP german RP server
+-- LAST EDIT by just_Salkin 15.11.2023 | WHITE-SANDS-RP german RP server
